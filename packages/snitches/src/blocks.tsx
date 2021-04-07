3a140d18549999fb323ef4adfe9990cdb46e2938
Craig Cavalier <craigcav@gmail.com>
2021-04-07 15:56:48 -0400

    fix: nested @media expressions
    This PR fixes an issue with the `block` method, where nested @media rules were incorrectly identified as individual style rules, instead of part of the parent @media expression.

--- packages/snitches/src/blocks.tsx
+++ packages/snitches/src/blocks.tsx
@@ -2,5 +2,18 @@
  * Decomposes a css ruleset into declaration blocks.
  */
 export const blocks = (style: string): string[] => {
-  return style.toString().split(/(?<=\})(?=\.|@|\s:)/);
+  let opened = 0;
+  let closed = 0;
+  return style
+    .toString()
+    .split(/(?<=\})(?!})/)
+    .reduce((prev, next) => {
+      if (opened === closed) prev.push(next);
+      else prev[prev.length - 1] += next;
+
+      opened += (next.match(/{/g) || []).length;
+      closed += (next.match(/}/g) || []).length;
+
+      return prev;
+    }, [] as any);
 };
